7b489bae44f9331d75ff128e8d2e72a4c158b553
BarryLi159159 <li2976809148@gmail.com>
2025-04-27 16:04:13 -0400

    revised

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Home from "./sections/Home";
 import Biography from "./sections/Biography";
 import Experience from "./sections/Experience";
 import Projects from "./sections/Projects";
+import Resume from "./sections/Resume";
 
 import Contact from "./sections/Contact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Navbar = () => (
   <nav className="fixed top-0 left-0 w-full bg-white text-gray-900 shadow-md py-4 px-6 flex justify-between items-center z-50">
@@ -15,21 +17,32 @@ const Navbar = () => (
       <Link to="biography" smooth={true} duration={500} className="cursor-pointer hover:text-blue-500 transition">Biography</Link>
       <Link to="experience" smooth={true} duration={500} className="cursor-pointer hover:text-blue-500 transition">Experience</Link>
       <Link to="projects" smooth={true} duration={500} className="cursor-pointer hover:text-blue-500 transition">Projects</Link>
-      
+      <Link to="resume" smooth={true} duration={500} className="cursor-pointer hover:text-blue-500 transition">
+        Resume
+      </Link>
       <Link to="contact" smooth={true} duration={500} className="cursor-pointer hover:text-blue-500 transition">Contact</Link>
     </div>
   </nav>
 );
 
 const App = () => (
-  <div className="bg-gray-100">
-    <Navbar />
-    <Home />
-    <Biography />
-    <Experience />
-    <Projects />
-    <Contact />
-  </div>
+  <Router>
+    <Routes>
+      <Route path="/" element={
+        <div className="bg-gray-100">
+          <Navbar />
+          <Home />
+          <Biography />
+          
+          <Experience />
+          
+          <Projects />
+          <Resume /> 
+          <Contact />
+        </div>
+      } />
+    </Routes>
+  </Router>
 );
 
 export default App;
